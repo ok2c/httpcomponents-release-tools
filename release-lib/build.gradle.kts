469516346e57479118542934d73988e8a3617d4d
Oleg Kalnichevski <olegk@apache.org>
2023-12-14 15:57:56 +0100

    Upgraded dependency declaration to Gradle Version Catalog

--- release-lib/build.gradle.kts
+++ release-lib/build.gradle.kts
@@ -23,34 +23,22 @@ group = "com.github.ok2c.hc.releasetools"
 version = "1-SNAPSHOT"
 
 repositories {
-    jcenter()
-}
-
-object Versions {
-    const val dom4j = "2.1.3"
-    const val `pull-parser` = "2.1.10"
-    const val jgit = "4.5.3.201708160445-r"
-    const val svnkit = "1.10.11"
-    const val tagsoup = "1.2.1"
-    const val slf4j = "1.7.30"
-    const val `junit-jupiter` = "5.6.0"
-    const val mockito = "2.28.2"
-    const val assertj = "3.15.0"
+    mavenCentral()
 }
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation("org.dom4j:dom4j:${Versions.dom4j}")
-    implementation("pull-parser:pull-parser:${Versions.`pull-parser`}")
-    implementation("org.eclipse.jgit:org.eclipse.jgit:${Versions.jgit}")
-    implementation("org.tmatesoft.svnkit:svnkit:${Versions.svnkit}")
-    implementation("org.tmatesoft.svnkit:svnkit-cli:${Versions.svnkit}")
-    implementation("org.ccil.cowan.tagsoup:tagsoup:${Versions.tagsoup}")
-    implementation("org.slf4j:slf4j-api:${Versions.slf4j}")
-    runtimeOnly("org.slf4j:slf4j-simple:${Versions.slf4j}")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:${Versions.`junit-jupiter`}")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:${Versions.`junit-jupiter`}")
-    testImplementation("org.assertj:assertj-core:${Versions.assertj}")
+    implementation(libs.dom4j)
+    implementation(libs.pullparser)
+    implementation(libs.jgit)
+    implementation(libs.svnkit)
+    implementation(libs.svnkit.cli)
+    implementation(libs.tagsoup)
+    implementation(libs.slf4j.api)
+    runtimeOnly(libs.slf4j.simple)
+    testImplementation(libs.junit.engine)
+    testImplementation(libs.junit.params)
+    testImplementation(libs.assertj.core)
 }
 
 tasks.test {
